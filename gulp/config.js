7b6069596ae7c158e2c4018aaf63754b7942905c
Bruno Macedo <contato@brunomacedo.com.br>
2018-07-21 13:54:35 -0300

    [GULP] module config

---
+++ gulp/config.js
@@ -0,0 +1,12 @@
+const config = {
+    scss: {
+        root: 'assets/scss/*.scss',
+        dest: 'public/css'
+    },
+    js: {
+        root: 'assets/js/*.js',
+        dest: 'public/js'
+    }
+};
+
+export { config };
