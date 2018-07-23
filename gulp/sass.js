24a86b96382db8a07d6f89a6070a23001101f90b
Bruno Macedo <contato@brunomacedo.com.br>
2018-07-23 16:11:35 -0300

    [GULP] update sass task to export minifed css

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,18 +1,28 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
+import rename from 'gulp-rename';
 
 import { config } from './config';
 import { error, good } from './logs';
 
 gulp.task('sass', () => {
+    console.log(error('Expanded sass on folder:', good(config.scss.dest)));
+    return gulp.src(config.scss.root)
+        .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest(config.scss.dest));
+});
+
+gulp.task('min-sass', () => {
     console.log(error('Compressed sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
-        .pipe(sass({ outputStyle: 'compressed' })) // expanded, compressed
         .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest(config.scss.dest));
 });
 
 gulp.task('style', () => {
-    gulp.watch(config.scss.root, ['sass']);
+    gulp.watch(config.scss.root, ['sass', 'min-sass']);
 });
