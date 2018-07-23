b2349729c3946de9e9db4de1870bfbdd8bba1d21
Bruno Macedo <contato@brunomacedo.com.br>
2018-07-23 15:33:23 -0300

    [GULP] two outputstyles

--- gulp/sass.js
+++ gulp/sass.js
@@ -8,7 +8,7 @@ import { error, good } from './logs';
 gulp.task('sass', () => {
     console.log(error('Compressed sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ outputStyle: 'compressed' })) // expanded, compressed
         .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
         .pipe(gulp.dest(config.scss.dest));
 });
