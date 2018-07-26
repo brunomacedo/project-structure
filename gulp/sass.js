acf0d72796e2f359d63137eb5f27a96116c53af6
Bruno Macedo <contato@brunomacedo.com.br>
2018-07-26 15:57:46 -0300

    fix sass error

--- gulp/sass.js
+++ gulp/sass.js
@@ -10,7 +10,7 @@ gulp.task('sass', () => {
     console.log(error('Expanded sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
         .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
-        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest(config.scss.dest));
 });
 
@@ -18,7 +18,7 @@ gulp.task('min-sass', () => {
     console.log(error('Compressed sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
         .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest(config.scss.dest));
 });
