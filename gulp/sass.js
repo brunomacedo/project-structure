786ab5638aae01e739e05094aa943490fcfd4d01
Bruno Macedo <contato@brunomacedo.com.br>
2018-07-26 17:14:43 -0300

    [GULP] fix sass reload on error

--- gulp/sass.js
+++ gulp/sass.js
@@ -9,16 +9,16 @@ import { error, good } from './logs';
 gulp.task('sass', () => {
     console.log(error('Expanded sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
-        .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
         .pipe(gulp.dest(config.scss.dest));
 });
 
 gulp.task('min-sass', () => {
     console.log(error('Compressed sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
-        .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest(config.scss.dest));
 });
