9e0dff5206802d7f4e6a12d60d2004af7bca4cd4
Bruno Macedo <contato@brunomacedo.com.br>
2018-08-16 17:10:15 -0300

    update watch task index

--- gulp/index.js
+++ gulp/index.js
@@ -1,7 +1,21 @@
 import gulp from 'gulp';
+import browserSync from 'browser-sync';
 
 import './sass';
 import './babel';
+import './template';
 
-gulp.task('default', ['dev', 'style']);
+import { config } from './config';
+
+gulp.task('default', ['template', 'build', 'sass', 'min-sass'], () => {
+    browserSync({
+        server: './public'
+    });
+
+    // .pipe(browserSync.stream())
+    gulp.watch(config.js.root, ['build']).on('change', browserSync.reload);
+    gulp.watch(config.scss.root, ['sass', 'min-sass']).on('change', browserSync.reload);
+    gulp.watch('src/**/*.html', ['template']).on('change', browserSync.reload);
+});
+
 gulp.task('publish', ['build', 'sass', 'min-sass']);
