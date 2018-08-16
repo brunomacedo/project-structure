6ab0a53439b6688367478dcbdea7836344f0bb33
Bruno Macedo <contato@brunomacedo.com.br>
2018-08-16 17:10:42 -0300

    update watch task scss

--- gulp/sass.js
+++ gulp/sass.js
@@ -6,14 +6,6 @@ import rename from 'gulp-rename';
 import { config } from './config';
 import { error, good } from './logs';
 
-gulp.task('sass', () => {
-    console.log(error('Expanded sass on folder:', good(config.scss.dest)));
-    return gulp.src(config.scss.root)
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-        .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
-        .pipe(gulp.dest(config.scss.dest));
-});
-
 gulp.task('min-sass', () => {
     console.log(error('Compressed sass on folder:', good(config.scss.dest)));
     return gulp.src(config.scss.root)
@@ -21,8 +13,4 @@ gulp.task('min-sass', () => {
         .pipe(autoprefixer({ browsers: ['last 40 versions', '> 5%', 'Firefox ESR'] }))
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest(config.scss.dest));
-});
-
-gulp.task('style', () => {
-    gulp.watch(config.scss.root, ['sass', 'min-sass']);
 });
